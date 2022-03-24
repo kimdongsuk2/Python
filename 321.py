b65f1bc005d3673420538360ea032eb588c227ea
KimDongSuk <tkaksek3@naver.com>
2022-03-24 14:34:52 +0900

    python file add

---
+++ 321.py
@@ -0,0 +1,11 @@
+sel =int(input("입력 진수 결정"(22/3/21):"")
+num=input(값 입력:)
+if sel==22:
+    num10-int(num,22)
+if sel==3:
+    num10 - int(num, 3)
+if sel==21:
+    num10 - int(num, 21)
+
+print("22진수==>",hex((num))
+print("3진수==>",hex((num))
