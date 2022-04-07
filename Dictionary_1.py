32e50abdfda74da15dac81a8790fef488b2adc87
dong suk <tkaksek3@naver.com>
2022-04-07 15:47:34 +0900

    pyhhom file add

---
+++ Dictionary_1.py
@@ -0,0 +1,9 @@
+singer ={}
+
+singer['이름']='여자친구'
+singer['구성원 수']=6
+singer['데뷔']='Season of Glass'
+singer['대표곡']='MAGO'
+
+for k in singer.keys():
+    print('%s==>%s' % (k,singer[k]))
---
+++ for_moon1.py
@@ -0,0 +1,20 @@
+fruit =['apple','banan','mango','melon']
+fruit.append("strawberry")
+print(len(fruit))
+fruit.pop()
+print(len(fruit))
+fruit.pop()
+
+
+j=0
+for i in fruit:
+    print(fruit.index(i),i)
+
+    #if 'apple' in fruit:
+    #    print('buy')
+   # else:
+
+    #print(fruit.index())
+
+   # print(j,fruit[j])
+   # j=j+1
